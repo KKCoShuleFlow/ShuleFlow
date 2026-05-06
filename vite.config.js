fbfd93ca8c64af5bf078efbf5e7c2f78b12d8630
Swordofdavid <tololotoo@gmail.com>
2026-05-06 18:20:02 +0300

    remove next.config.js (conflicting with vite)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      // this will surface path/casing errors
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{js,jsx}"'
+      }
+    })
+  ]
 })
