3bffc1224894aca2f7e2ec6c98c3b5a96dae90fc
Swordofdavid <tololotoo@gmail.com>
2026-05-06 17:18:13 +0300

    remove next-pwa (not compatible with vite)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import Layout from "./components/layout"
+import Layout from "./components/Layout"
 
 // PAGES
 import HomeDashboard from "./pages/HomeDashboard"
